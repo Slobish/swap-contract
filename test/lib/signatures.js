b65f7c64f8cadc86a435f4325933f90b4d4c810b
Don Mosites <mosites@gmail.com>
2019-03-04 17:17:29 -0500

    Comment update for EIP191 versions

--- test/lib/signatures.js
+++ test/lib/signatures.js
@@ -10,7 +10,7 @@ module.exports = {
     const sig = await web3.eth.sign(orderHashHex, signer)
     const { v, r, s } = ethUtil.fromRpcSig(sig)
     return {
-      version: '0x45', // peronal_sign
+      version: '0x45', // Version 0x45: personal_sign
       v,
       r,
       s,
@@ -21,7 +21,7 @@ module.exports = {
     const orderHashBuff = ethUtil.toBuffer(orderHash)
     const { r, s, v } = ethUtil.ecsign(orderHashBuff, privateKey)
     return {
-      version: '0x01', // signTypedData
+      version: '0x01', // Version 0x01: signTypedData
       v,
       r,
       s,
@@ -42,15 +42,15 @@ module.exports = {
     })
     const { v, r, s } = ethUtil.fromRpcSig(sig)
     return {
-      version: '0x01', // signTypedData
+      version: '0x01', // Version 0x01: signTypedData
       v,
       r,
       s,
     }
   },
   async getLegacySignature(order, signer, verifyingContract) {
-    // using EIP 191 signature scheme version 0 (intended validator)
     const msg = web3.utils.soliditySha3(
+      // Version 0x00: Data with intended validator (verifyingContract)
       { type: 'bytes1', value: '0x0' },
       { type: 'address', value: verifyingContract },
       { type: 'address', value: order.maker.wallet },
